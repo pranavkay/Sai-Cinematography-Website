7e49cc868084b6575772ef44b1afbd966a0f9786
Pranav Kay <140229452+pranavkay@users.noreply.github.com>
2025-12-12 12:08:18 +0530

    Update index.tsx
    browserRouter -> hashRouter

--- index.tsx
+++ index.tsx
@@ -2,14 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-// 1. Import the Router
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* 2. Add the Router with the basename */}
-    <BrowserRouter basename="/Sai-Cinematography-Website">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
